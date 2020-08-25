5c9d2dec58f37e140cb52a3041856c712decd4bd
Sungwook <funforwook@likelion.org>
2020-08-25 22:27:47 +0900

    create profile and login

--- gingerproject/urls.py
+++ gingerproject/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from blog.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     path('', include('blog.urls')),
+    path('', include('accounts.urls')),
+    path('accounts/', include('allauth.urls')),    
 ]
+
