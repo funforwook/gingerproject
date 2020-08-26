b5ca5297972adbc6088802db5808546c9e73a967
이승혜 <shoongszzi@iseunghyeui-MacBookPro.local>
2020-08-26 11:50:28 +0900

    메인crudstage1

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.conf import settings #유저모델
 
 # Create your models here.
 class Favorite(models.Model):
@@ -9,3 +10,12 @@ class Favorite(models.Model):
 
 class Save(models.Model):
     created_at = models.DateTimeField('작성시간', default=timezone.now)
+
+class Add(models.Model):
+    objects = models.Manager()
+    link = models.URLField('링크')
+    tag = models.CharField('태그',max_length=200,null=True)
+    comments = models.CharField("코멘트", max_length=200,null=True)
+
+    def __str__(self):
+        return self.link
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,53 @@
-from django.shortcuts import render
-from .models import Favorite, Save
+from django.shortcuts import render, redirect
+from .models import Add
+from .forms import AddForms
+from django.utils import timezone
 
 # Create your views here.
 def index(request):
     context=dict()
+    all_add = Add.objects.all()
+    context['all_add'] = all_add
     return render(request,'index.html',context)
 
-def create(rqeuqest):
+def create(request):
     context=dict()
-    return redner(request, 'index.html',context)
+    if request.method == "POST":
+        temp_form = AddForms(request.POST)
+        if temp_form.is_valid():
+            #add_form = temp_form.save(commit=False) #담아만두기
+            #add_form.date = timezone.now() #작성시간만들어주기
+            temp_form.save()
+            return redirect('index')
+        else:
+            context["write_form"] = temp_form
+            return render(request,'add_cont.html',context)            
+    else:
+        context["write_form"] = AddForms()
+        return render(request,'add_cont.html',context)
+        
+def detail(request, add_id):
+    context= dict()
+    context['detail_add'] = Add.objects.get(id = add_id)
+    return render(request, 'detail.html', context)
+
+def update(request, add_id):
+    context= dict()
+    if request.method == "POST":
+        temp_form = AddForms(request.POST,instance=Add.objects.get(id = add_id))
+        if temp_form.is_valid():
+            add_form = temp_form.save(commit=False) #담아만두기
+            add_form.date = timezone.now() #작성시간만들어주기
+            temp_form.save()
+            return redirect('index')
+        else:
+            context["write_form"] = temp_form
+            return render(request,'add_cont.html',context)            
+    else:
+        context["write_form"] = AddForms(instance=Add.objects.get(id = add_id))
+        return render(request,'add_cont.html',context)
+
+def delete(request, add_id):
+    delete_form = Add.objects.get(id = add_id)
+    delete_form.delete()
+    return redirect('index')
